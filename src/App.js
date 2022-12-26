cec782a4c9c9439ae423b936b94898795bf33178
Atharva Pakade <pakade310@gmail.com>
2022-12-26 19:05:53 +0530

    removed chakra boilerplate components

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import {
   theme,
 } from '@chakra-ui/react';
 
-import { Logo } from './Logo';
-
 import { Navbar } from './components/Navbar';
 import Home from './components/Home';
 import Schedule from './components/schedule/Schedule';
