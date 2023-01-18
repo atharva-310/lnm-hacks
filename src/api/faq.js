28c23f6767b19d06fe4ca9913aa90cb0e8a4a0e1
Ramsha42001 <ramshaiqbal4@gmail.com>
2023-01-18 14:29:39 +0530

    updated faq questions

--- src/api/faq.js
+++ src/api/faq.js
@@ -1,24 +1,43 @@
 export const question = [
   {
     id: 1,
-    question: 'Team size',
-    sol: '1-4',
+    question: 'Can I participate remotely?',
+    sol: 'LNM Hacks is an in person hackathon.So remote participation is not applicable.',
   },
   {
     id: 2,
-    question: 'Team size',
-    sol: '1-4',
+    question: 'I am a beginner , can I participate?',
+    sol: 'Absolutely,along with several workshops targeted for beginners we have mentors and resources. Come learn and experience your first hackathon.',
   },
   {
     id: 3,
-    question: 'Team size',
-    sol: '1-4',
+    question: 'Will you support or reimburse our travel expenses?',
+    sol: 'LNM hacks is a community hackathon, so unfortunately we would not be able to support you with travel.',
   },
   {
     id: 4,
-    question: 'Team size',
-    sol: '1-4',
+    question: 'How much does it cost to participate?',
+    sol: 'Nothing, participation in LNM hacks is absolutely FREE!!🎉 We’ll have meals, snacks, and beverages onsite at the hackathon, in addition to swags, prizes, fun mini-events and more.',
   },
+
+  {
+    id: 5,
+    question: ' What is the maximum team size? Can I participate as a Solo Hacker?',
+    sol: 'We encourage you to make a team of minimum 2 and maximum 4 members. If you are a Solo Hacker and do not have a team, you can still register and we will help you find a team during the hackathon.',
+  },
+
+  {
+    id: 6,
+    question: 'What are the sleeping',
+    sol: 'We will have a supervised sleeping space for you to rest and sleep at the venue. We recommend bringing your own blanket if you need one.',
+  },
+
+  {
+    id: 7,
+    question: 'My parent/guardian will be coming along with me. Can they stay at the venue?',
+    sol:'The answer is No. Arrangements like sleeping area, food, etc., are only for the attendees. We recommend you to find a stay option for them near the venue. They can drop you at the hackathon venue on 3rd Feb and can pick you on 5th Feb but can not stay at venue during the hackathon as we do not have any provision for them.'
+
+  }
 ];
 
 // <AccordionItem my={3}>
