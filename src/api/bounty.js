919964f46866d503cc63b843cd654e97dcec2e5f
Ramsha42001 <ramshaiqbal4@gmail.com>
2023-01-26 14:53:49 +0530

    added bounty and sponsers

--- src/api/bounty.js
+++ src/api/bounty.js
@@ -3,79 +3,92 @@ import rep from '../assets/cur-sponsor-logo/Replit-light-background.png';
 import sola from '../assets/cur-sponsor-logo/Solana-Colored.png';
 import huddle from '../assets/cur-sponsor-logo/huddle01-logo-blue.svg';
 import poly from '../assets/cur-sponsor-logo/Polygon_Logo-Colored@2x.png';
-
+import deep from '../assets/cur-sponsor-logo/deepVerse.jpg';
+import spheron from '../assets/cur-sponsor-logo/spheron.jpg';
+import tezo from '../assets/cur-sponsor-logo/tezo.png';
+import gari from '../assets/cur-sponsor-logo/gari.jpg';
+import openInn from '../assets/cur-sponsor-logo/openIno.png'
+import xdc from '../assets/cur-sponsor-logo/xdc.png';
 export const bounty = [
   {
     id: 1,
-    name: 'Deepverse',
-    logo: file,
-    link: 'https://filecoin.io/',
-    bounty: '$200',
+    name: 'Open Innovation',
+    logo: openInn,
+    //link: 'https://solana.com/',
+    bounty: '$1500',
   },
   {
     id: 2,
+    name: 'Solana',
+    logo: sola,
+    link: 'https://solana.com/',
+    bounty: '$850',
+  },
+  {
+    id: 3,
+    name: 'Gari',
+    logo: gari,
+    link: 'https://gari.network/',
+    bounty: '$800',
+  },
+  
+  {
+    id: 4,
+    name: 'Tezos',
+    logo: tezo,
+    link: 'https://tezos.com/',
+    bounty: '$800',
+  },
+  {
+    id: 5,
+    name: 'Spheron',
+    logo: spheron,
+    link: 'https://spheron.network/',
+    bounty: '$800',
+  },
+  {
+    id: 6,
+    name: 'XDC',
+    logo: xdc,
+    link: 'https://xinfin.org/',
+    bounty: '$500',
+  },
+  {
+    id: 7,
     name: 'Huddle01',
     logo: huddle,
     link: 'https://www.huddle01.com/',
     bounty: '$500',
   },
   {
-    id: 3,
-    name: 'Spheron',
-    logo: sola,
-    link: 'https://solana.com/',
-    bounty: '$500',
-  },
-
-  {
-    id: 4,
-    name: 'Tezos',
-    logo: sola,
-    link: 'https://solana.com/',
-    bounty: '$200',
-  },
-
-  {
-    id: 5,
+    id: 8,
     name: 'Polygon',
     logo: poly,
     link: 'https://polygon.technology/',
     bounty: '$350',
   },
   {
-    id: 6,
-    name: 'Solana',
-    logo: sola,
-    link: 'https://solana.com/',
-    bounty: '$850',
+    id: 9,
+    name: 'Deepverse',
+    logo: deep,
+    link: 'https://mobile.twitter.com/daodeepverse',
+    bounty: '$300',
   },
 
   {
-    id: 7,
-    name: 'Gari',
-    logo: sola,
-    link: 'https://solana.com/',
-    bounty: '$200',
-  },
-  {
-    id: 8,
+    id: 10,
     name: 'Filecoin',
     logo: file,
     link: 'https://filecoin.io/',
     bounty: '$250',
   },
   {
-    id: 9,
+    id: 11,
     name: 'Replit',
     logo: sola,
-    link: 'https://solana.com/',
-    bounty: '$200',
+    link:  'https://replit.com/',
+    bounty: '$50',
   },
-  {
-    id: 10,
-    name: 'Open Innovation',
-    logo: sola,
-    link: 'https://solana.com/',
-    bounty: '$200',
-  },
+
+
 ];
