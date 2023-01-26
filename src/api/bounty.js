fddc5612bb0d283e5ca2941b66404738b150ac1a
Ramsha42001 <ramshaiqbal4@gmail.com>
2023-01-26 14:06:08 +0530

    added bounty

--- src/api/bounty.js
+++ src/api/bounty.js
@@ -1,25 +1,79 @@
 import file from '../assets/cur-sponsor-logo/filecoin.png';
 import rep from '../assets/cur-sponsor-logo/Replit-light-background.png';
 import sola from '../assets/cur-sponsor-logo/Solana-Colored.png';
+import huddle from '../assets/cur-sponsor-logo/huddle01-logo-blue.svg';
+import poly from '../assets/cur-sponsor-logo/Polygon_Logo-Colored@2x.png';
 
 export const bounty = [
   {
     id: 1,
-    name: 'Filecoin',
+    name: 'Deepverse',
     logo: file,
     link: 'https://filecoin.io/',
     bounty: '$200',
   },
   {
     id: 2,
-    name: 'Replit',
-    logo: rep,
-    link: 'https://replit.com/',
-    bounty: '$200',
+    name: 'Huddle01',
+    logo: huddle,
+    link: 'https://www.huddle01.com/',
+    bounty: '$500',
   },
   {
     id: 3,
+    name: 'Spheron',
+    logo: sola,
+    link: 'https://solana.com/',
+    bounty: '$500',
+  },
+
+  {
+    id: 4,
+    name: 'Tezos',
+    logo: sola,
+    link: 'https://solana.com/',
+    bounty: '$200',
+  },
+
+  {
+    id: 5,
+    name: 'Polygon',
+    logo: poly,
+    link: 'https://polygon.technology/',
+    bounty: '$350',
+  },
+  {
+    id: 6,
     name: 'Solana',
+    logo: sola,
+    link: 'https://solana.com/',
+    bounty: '$850',
+  },
+
+  {
+    id: 7,
+    name: 'Gari',
+    logo: sola,
+    link: 'https://solana.com/',
+    bounty: '$200',
+  },
+  {
+    id: 8,
+    name: 'Filecoin',
+    logo: file,
+    link: 'https://filecoin.io/',
+    bounty: '$250',
+  },
+  {
+    id: 9,
+    name: 'Replit',
+    logo: sola,
+    link: 'https://solana.com/',
+    bounty: '$200',
+  },
+  {
+    id: 10,
+    name: 'Open Innovation',
     logo: sola,
     link: 'https://solana.com/',
     bounty: '$200',
